739cad90b460ec3bfe9bfa94d5b875160bb9361e
G-bit-maker <76432838+G-bit-maker@users.noreply.github.com>
2021-03-19 12:40:08 +0530

    fd

---
+++ FrontEnd/src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: true,
+  // baseUrl: 'http://15.185.182.194:3000/',
+  // baseUrl: 'http://15.185.182.194/dev/'
+  baseUrl: 'http://65.1.122.8/dev/'
+
+};
